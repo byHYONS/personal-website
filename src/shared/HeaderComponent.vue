2ac00c5662feea329a66286b2ac0a10f121fadfe
byHYONS <byhyons@gmail.com>
2024-12-12 11:03:29 +0100

    FEAT: primo commit

--- src/shared/HeaderComponent.vue
+++ src/shared/HeaderComponent.vue
@@ -8,9 +8,27 @@ export default {
    data(){
       return{
         store,
+        scroll: false,
 
       }
    },
+  methods: {
+    topHeader(){
+      this.scroll = window.scrollY > 100
+      if (this.scroll) {
+      document.querySelector('nav').className = 'float';
+      }
+      else {
+        document .querySelector('nav').className = 'navbar navbar-expand-md navbar-light bg-white shadow-sm';
+      }
+    }
+  },
+  mounted(){
+    window.addEventListener("scroll", this.topHeader)
+  },
+  beforeDestroy() {
+    window.remuveEventListener("scroll", this.topHeader)
+  },
 }
 
 </script>
@@ -20,31 +38,11 @@ export default {
 <template>
   
   <header>    
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <nav  class="no-float">
             <div class="container">
                   <router-link :to="{name: 'home'}" class="navbar-brand d-flex align-items-center">
                     <div class="logo_byhyons">
-                        <svg width="60%" height="60%" viewBox="0 0 33334 14584" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:square;">
-                            <g transform="matrix(4.16667,0,0,4.16667,0,0)">
-                                <g id="document" transform="matrix(1.95661,0,0,1.95661,4007.14,1750)">
-                                    <g>
-                                        <path d="M-1067.02,583.743L-1659.59,583.743L-1659.59,-8.826C-1659.59,-331.288 -1399,-591.876 -1076.54,-591.876L-483.967,-591.876L-483.967,0.693C-483.967,323.155 -744.555,583.743 -1067.02,583.743Z" style="fill:none;fill-rule:nonzero;stroke:rgb(230,0,0);stroke-width:26.04px;"/>
-                                        <path d="M-569.818,504.641L-569.818,-500.298L-971.793,-500.298L-971.793,-56.443L-1172.78,-56.443L-1172.78,-500.298L-1574.76,-500.298L-1574.76,504.641L-1172.78,504.641L-1172.78,180.718L-971.793,180.718L-971.793,504.641L-569.818,504.641Z" style="fill:white;fill-rule:nonzero;stroke:rgb(190,190,190);stroke-width:26.04px;"/>
-                                        <g>
-                                            <path d="M-1604.28,-439.929L-1604.28,-430.419C-1604.28,-417.037 -1615.08,-406.234 -1628.47,-406.234L-1668.77,-406.234L-1668.77,-390.111L-1628.47,-390.111C-1606.22,-390.111 -1588.16,-408.169 -1588.16,-430.419L-1588.16,-512.478L-1604.28,-512.478L-1604.28,-472.171C-1604.28,-458.789 -1615.08,-447.986 -1628.47,-447.986C-1641.85,-447.986 -1652.65,-458.789 -1652.65,-472.171L-1652.65,-512.478L-1668.77,-512.478L-1668.77,-472.171C-1668.77,-449.921 -1650.72,-431.863 -1628.47,-431.863C-1619.39,-431.863 -1611.02,-434.865 -1604.28,-439.929Z" style="fill-rule:nonzero;"/>
-                                            <path d="M-1755.63,-470.727C-1754.87,-449.145 -1737.12,-431.863 -1715.35,-431.863C-1693.1,-431.863 -1675.04,-449.921 -1675.04,-472.171C-1675.04,-494.42 -1693.1,-512.478 -1715.35,-512.478C-1724.43,-512.478 -1732.8,-509.475 -1739.54,-504.41L-1739.54,-600.203L-1755.66,-600.203L-1755.66,-470.727L-1755.63,-470.727ZM-1715.35,-496.355C-1701.97,-496.355 -1691.17,-485.553 -1691.17,-472.171C-1691.17,-458.789 -1701.97,-447.986 -1715.35,-447.986C-1728.73,-447.986 -1739.54,-458.789 -1739.54,-472.171C-1739.54,-485.553 -1728.73,-496.355 -1715.35,-496.355Z" style="fill-rule:nonzero;"/>
-                                        </g>
-                                        <g>
-                                            <path d="M-77.076,349.817L-77.076,368.51C-77.076,443.835 -137.881,504.64 -213.206,504.64L-569.818,504.64L-569.818,598.313L-213.206,595.394C-87.966,595.394 13.678,493.75 13.678,368.51L13.678,-58.55L-77.076,-58.55L-77.076,168.333C-77.076,243.658 -137.881,304.463 -213.206,304.463C-288.531,304.463 -349.336,243.658 -349.336,168.333L-349.336,-58.55L-440.089,-58.55L-440.089,168.333C-440.089,293.573 -338.445,395.217 -213.206,395.217C-162.143,395.217 -115.003,378.32 -77.076,349.817Z" style="fill-rule:nonzero;stroke:rgb(190,190,190);stroke-width:13.05px;"/>
-                                            <path d="M509.208,168.333C509.208,293.573 407.564,395.217 282.325,395.217C157.085,395.217 55.442,293.573 55.442,168.333C55.442,43.094 157.085,-58.55 282.325,-58.55C407.564,-58.55 509.208,43.094 509.208,168.333ZM282.325,32.203C207,32.203 146.195,93.008 146.195,168.333C146.195,243.658 207,304.463 282.325,304.463C357.65,304.463 418.455,243.658 418.455,168.333C418.455,93.008 357.65,32.203 282.325,32.203Z" style="fill-rule:nonzero;stroke:rgb(190,190,190);stroke-width:13.05px;"/>
-                                            <path d="M643.841,-11.044L643.841,-56.444L553.087,-56.444L553.087,397.323L643.841,397.323L643.841,170.44C643.841,95.115 704.645,34.31 779.971,34.31C855.296,34.31 916.101,95.115 916.101,170.44L916.101,397.323L1006.85,397.323L1006.85,170.44C1006.85,45.2 905.21,-56.444 779.971,-56.444C728.908,-56.444 681.768,-39.547 643.841,-11.044Z" style="fill-rule:nonzero;stroke:rgb(190,190,190);stroke-width:13.05px;"/>
-                                            <path d="M1504.45,34.31L1186.82,34.31C1161.86,34.31 1141.44,54.729 1141.44,79.687C1141.44,104.644 1161.86,125.063 1186.82,125.063L1368.32,125.063C1443.65,125.063 1504.45,185.868 1504.45,261.193C1504.45,336.518 1443.65,397.323 1368.32,397.323L1050.69,397.323L1050.69,306.57L1368.32,306.57C1393.28,306.57 1413.7,286.15 1413.7,261.193C1413.7,236.236 1393.28,215.817 1368.32,215.817L1186.82,215.817C1111.49,215.817 1050.69,155.012 1050.69,79.687C1050.69,4.361 1111.49,-56.444 1186.82,-56.444L1504.45,-56.444L1504.45,34.31Z" style="fill-rule:nonzero;stroke:rgb(190,190,190);stroke-width:13.05px;"/>
-                                        </g>
-                                        <path d="M1629.24,-194.293L1552.06,-140.769L1579.27,-230.37L1504.45,-287.147L1598.49,-289.217L1629.24,-377.931L1660,-289.217L1754.03,-287.147L1679.22,-230.37L1706.42,-140.769L1629.24,-194.293Z" style="fill:rgb(190,190,190);fill-rule:nonzero;stroke:black;stroke-width:3.25px;stroke-linecap:round;stroke-linejoin:round;"/>
-                                    </g>
-                                </g>
-                            </g>
-                        </svg>
+                      <svg  translate="no" width="100%" height="100%" viewBox="0 0 6513 1955" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><text x="1892.04px" y="1532.67px" style="font-family:'STHeiti';font-size:833.333px;fill:#161f28;stroke:#cad0d9;stroke-width:4.17px;">PARATORE</text><rect x="716.429" y="821.35" width="483.829" height="762.946" style="fill:#cad0d9;stroke:#fff;stroke-width:3.96px;stroke-linecap:square;stroke-miterlimit:1.5;"/><path d="M340.796,1573.52l235.675,0.033l1.017,-343.446l751.716,-356.608l0.529,-310.875l-355.087,-0.371l0.671,902.483l698.975,0.729l0.266,-300.27l-344.391,-216.105l0.196,623.992l676.816,0.425" style="fill:none;stroke:#161f28;stroke-width:33.33px;stroke-linecap:square;stroke-miterlimit:1.5;"/><text x="1959.62px" y="721.608px" style="font-family:'STHeiti';font-size:416.667px;fill:#161f28;">SAMUELE</text></svg>
                     </div>
                   </router-link>
                 <!--? nav bar -->
@@ -73,6 +71,32 @@ export default {
 header {
   position: sticky;
   top: 0;
+
+  .float {
+  position: fixed;
+  top: 35px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 90%;
+  margin: 5px auto;
+  border-radius: 55px;
+  background-color: #fff;
+  box-shadow: 5px 5px 5px rgba(80, 80, 80, 0.3);
+  color: #fff;
+  font-size: 20px;
+  font-weight: 500;
+  padding: 15px 45px;
+  transition:
+    opacity 0.5s ease-in-out,
+    visibility 0.5s ease-in-out;
+  }
+  .no-float{
+  transition:
+    opacity 0.5s ease-in-out,
+    visibility 0.5s ease-in-out;
+
+  }
+  
   #app {
     height: 100px;
   }
